cf4ce003c0b390ecb31b2d23a2224b7c386417a1
BenjaPalacios <107632106+BenjaPalacios@users.noreply.github.com>
2022-08-15 23:55:38 -0500

    Add files via upload

---
+++ HolaMundo/src/main.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Benjamin
+ */
+public class main {
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null,"Hola Mundo");
+    }
+}
